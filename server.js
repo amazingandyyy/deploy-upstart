c10347109c11c1991717bc2d6fd7d1b75a45f7fd
Amazingandyyy <amazingandyyy@gmail.com>
2017-01-06 23:27:13 -0800

    change index.html

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
 } else {
     app.use(express.static('dist'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'dist/index.html'))
+        res.sendFile(path.join(__dirname, 'dist', 'index.html'))
     })
 }
 
